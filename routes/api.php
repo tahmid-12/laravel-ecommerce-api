a5205f92066ac85c673536024dd663196e06e3e0
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2024-07-14 23:57:31 +0600

    Some route setup

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('products',function(){
+    return 'Post Products';
+});
+
+Route::get('products',function(){
+    return 'Get Products';
+});
+
+Route::get('products/{id}',function($id){
+    return 'Get Products '. $id;
+});
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
